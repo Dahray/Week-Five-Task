13cb704a436836ed0a2b71b0d1a7785bb6d3f5ec
Dahray <Dahrayafolabi@gmail.com>
2022-11-29 21:55:29 +0100

    Newly edit

--- song_CRUD/musicapp/models.py
+++ song_CRUD/musicapp/models.py
@@ -8,7 +8,7 @@ class Artist(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     age = models.IntegerField()
-    
+        
     def __str__(self):
         return self.first_name
 
